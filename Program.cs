ef0e1247ea3f0c427d2bc900e67da6ea3628d16d
popontdev <popontstudios@gmail.com>
2020-07-31 20:53:51 +0200

    commer

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace matejin_c_sharpener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("me sad bye");
+        }
+    }
+}
